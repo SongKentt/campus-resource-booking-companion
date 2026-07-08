3c01f2fa53c0d38508b5a9fddbb7de278029eefe
Song Kent <leongsongkent@gmail.com>
2026-07-08 19:06:33 +0800

    DataStorage Class Update

--- reference-javafx-client/src/main/java/com/campus/client/model/DataStorage.java
+++ reference-javafx-client/src/main/java/com/campus/client/model/DataStorage.java
@@ -1,5 +1,135 @@
 package com.campus.client.model;
 
-public class DataStorage {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
+public class DataStorage {
+    private final String userDataFilePath;
+    private final String bookingRecordFilePath;
+
+    public DataStorage(String userDataFilePath, String bookingHistoryFilePath) {
+        this.userDataFilePath = userDataFilePath;
+        this.bookingRecordFilePath = bookingHistoryFilePath;
+    }
+
+    public ArrayList<Student> loadStudents() {
+        ArrayList<Student> studentsList = new ArrayList<>();
+        Path path = Paths.get(userDataFilePath);
+        if (!Files.exists(path)) {
+            return studentsList;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(userDataFilePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+
+                String[] parts = line.split("\\|");
+                if (parts.length == 3) {
+                    studentsList.add(new Student(parts[0].trim(), parts[1].trim(), parts[2].trim()));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading student file: " + e.getMessage());
+        }
+        return studentsList;
+    }
+
+    public ArrayList<Booking> loadBookings() {
+        ArrayList<Booking> bookingRecordList = new ArrayList<>();
+        Path path = Paths.get(bookingRecordFilePath);
+        if (!Files.exists(path)) {
+            return bookingRecordList;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(bookingRecordFilePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+                String[] parts = line.split("\\|");
+                if (parts.length == 7) {
+                    bookingRecordList.add(new Booking(
+                            parts[0].trim(), parts[1].trim(), LocalDate.parse(parts[2].trim()),
+                            LocalTime.parse(parts[3].trim()), LocalTime.parse(parts[4].trim()),
+                            parts[5].trim(), Integer.parseInt(parts[6].trim())
+                    ));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading booking file: " + e.getMessage());
+        }
+
+        return bookingRecordList;
+    }
+
+    public void saveBooking(Booking booking) {
+        Path path = Paths.get(bookingRecordFilePath);
+
+        String line = String.format("%s | %s | %s | %s | %s | %s | %d",
+                booking.getBookingRef(), booking.getResourceId(), booking.getDate(),
+                booking.getStartTime(), booking.getEndTime(), booking.getStudentId(),
+                booking.getStatus());
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(bookingRecordFilePath, true))) {
+            if (Files.size(path) == 0){
+                bw.write("# bookingRef | resourceId | date | start | end | studentId | status");
+                bw.newLine();
+            }
+
+            bw.write(line);
+            bw.newLine();
+        } catch (IOException e) {
+            System.err.println("Failed to save booking: " + e.getMessage());
+        }
+    }
+
+    public void updateBookingStatus(String bookingRef, int newStatus) {
+        ArrayList<Booking> allBookingRecord = loadBookings();
+        boolean bookingRecordExist = false;
+
+        for (Booking bookingRecord : allBookingRecord) {
+            if (bookingRecord.getBookingRef().equals(bookingRef)) {
+                bookingRecordExist = true;
+                bookingRecord.setStatus(newStatus);
+                break;
+            }
+        }
+
+        if (!bookingRecordExist) {
+            System.err.println("Booking doesn't exist: " + bookingRef);
+            return;
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(bookingRecordFilePath))) {
+
+            bw.write("# bookingRef | resourceId | date | start | end | studentId | status");
+            bw.newLine();
+
+            for (Booking bookingRecord : allBookingRecord) {
+                String line = String.format("%s | %s | %s | %s | %s | %s | %d",
+                        bookingRecord.getBookingRef(), bookingRecord.getResourceId(),
+                        bookingRecord.getDate(), bookingRecord.getStartTime(),
+                        bookingRecord.getEndTime(), bookingRecord.getStudentId(),
+                        bookingRecord.getStatus()
+                );
+                bw.write(line);
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Failed to update booking: " + e.getMessage());
+        }
+    }
 }
+
