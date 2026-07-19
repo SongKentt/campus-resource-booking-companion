23a39c0a6a155a42cecde5263c719e7358637f48
Song Kent <leongsongkent@gmail.com>
2026-07-19 11:28:43 +0800

    Finalize Student Class

--- reference-javafx-client/src/main/java/com/campus/client/model/Student.java
+++ reference-javafx-client/src/main/java/com/campus/client/model/Student.java
@@ -5,13 +5,14 @@ public class Student {
     private final String name;
     private final String password;
 
-
+    // Constructor of Booking, to initialize the attribute values of the student instance
     public Student(String studentId, String name, String password){
         this.studentId = studentId;
         this.name = name;
         this.password = password;
     }
 
+    // Getters methods for the student instance
     public String getStudentId(){
         return studentId;
     }
